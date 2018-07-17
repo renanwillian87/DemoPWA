addc82ec53311afebb8fb65fdb2017e2ca32ab67
renan santos <renan@314inovacao.com>
2018-07-16 22:50:02 -0300

    Create project and import PWA Essential

---
+++ DemoPWA/DemoPWA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemoPWA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
