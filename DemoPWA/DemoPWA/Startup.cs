d15966a475cda8adee11e3701f2db61dbfa8dfbd
renan santos <renan@314inovacao.com>
2018-07-18 22:50:54 -0300

    PWA

--- DemoPWA/DemoPWA/Startup.cs
+++ DemoPWA/DemoPWA/Startup.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebEssentials.AspNetCore.Pwa;
 
 namespace DemoPWA
 {
@@ -21,7 +19,15 @@ namespace DemoPWA
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddProgressiveWebApp(new PwaOptions
+            {
+                //CacheId = "Worker " + version,
+                Strategy = ServiceWorkerStrategy.CacheFirst,
+                RoutesToPreCache = "/Home/News"
+            });
+
             services.AddMvc();
+            services.AddSingleton(typeof(IHttpContextAccessor), typeof(HttpContextAccessor));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
